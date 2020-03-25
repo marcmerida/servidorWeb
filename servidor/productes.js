946ddd2287535d150ddd0ea529245a5448f0b5be
Alumne <marcerida5091@gmail.com>
2020-03-25 19:44:40 +0100

    Tarjetas de producto

--- servidor/productes.js
+++ servidor/productes.js
@@ -30,12 +30,31 @@ class Obj {
             try {
                 sql = 'CREATE TABLE productes (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, nom VARCHAR(50) NOT NULL, descripcio TEXT, preu INT(6), imatge VARCHAR(255))'
                 await db.promiseQuery(sql)
-                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Tele", "Tele molt xula", 800, "/web/imatges/producte-1.jpg")'
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Africa", "El contiente africano con una inmensa fauna", 800, "/web/imatges/africa.jpg")'
                 await db.promiseQuery(sql)
-                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Batidora", "Batidora molt xula", 20, "/web/imatges/producte-2.png")'
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Barcelona", "Una de las ciudades mas bonitas del Mediterraneo", 20, "/web/imatges/barcelona.jpg")'
                 await db.promiseQuery(sql)
-                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Aspirador", "Aspirador molt xulo", 300, "/web/imatges/producte-3.jpg")'
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Vietnam", "Un pais con una cultura muy variada", 300, "/web/imatges/vietnam.jpg")'
                 await db.promiseQuery(sql)
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Moscu", "La capital del pais mas grande del mundo", 300, "/web/imatges/moscu.jpeg")'
+                await db.promiseQuery(sql)
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Amsterdam", "La capital holandesa preferida del turismo joven", 300, "/web/imatges/amsterdam.jpg")'
+                await db.promiseQuery(sql)
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Berlin", "Una ciudad con mucha historia", 300, "/web/imatges/berlin.png")'
+                await db.promiseQuery(sql)
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Galicia", "Una comunidad autonoma del noroeste de España", 300, "/web/imatges/galicia.jpg")'
+                await db.promiseQuery(sql)
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Nueva York", "La ciudad favorita de los viajeros", 300, "/web/imatges/nuevayork.jpg")'
+                await db.promiseQuery(sql)
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Las Vegas", "La ciudad de los casinos", 300, "/web/imatges/lasvegas.jpg")'
+                await db.promiseQuery(sql)
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Los Angeles", "Una de las ciudades mas importantes donde se han grabado miles de peliculas", 300, "/web/imatges/losangeles.jpg")'
+                await db.promiseQuery(sql)
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Malaga", "Una ciudad española con mucho encanto", 300, "/web/imatges/malaga.jpg")'
+                await db.promiseQuery(sql)
+                
+                
+
             } catch (e) {
                 console.error(e)
                 return result.json({ resultat: "ko", missatge: "Error, funció llistatProductes: no s'ha pogut crear la taula productes"})  
